78eba883aad745e85f4ddebab82c3b8abf21535f
ethannissley <ethannissley96@gmail.com>
2018-03-25 21:21:06 -0700

    Create NewPerson.cs

---
+++ CommunityAssist2018/CommunityAssist2018/Models/NewPerson.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist2018.Models
+{
+    public class NewPerson
+    {
+        public string NewPersonLastName { get; set; }
+        public string NewPersonFirstName { get; set; }
+        public string NewPersonEmail { get; set; }
+        public string NewPersonPhone { get; set; }
+        public string NewPersonPlainPassword { get; set; }
+        public string NewPersonApartment { get; set; }
+        public string NewPersonStreet { get; set; }
+        public string NewPersonCity { get; set; }
+        public string NewPersonState { get; set; }
+        public string NewPersonZipcode { get; set; }
+
+
+    }
+
+}
