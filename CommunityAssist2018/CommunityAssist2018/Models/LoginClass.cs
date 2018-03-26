5237c8423a2d878bfabd7d87f124ba3d3c99d51a
ethannissley <ethannissley96@gmail.com>
2018-03-25 21:50:01 -0700

    Create LoginClass.cs

---
+++ CommunityAssist2018/CommunityAssist2018/Models/LoginClass.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssist2018.Models
+{
+    public class LoginClass
+    {
+        public LoginClass() { }
+        public LoginClass(string user, string password)
+        {
+            UserName = user;
+            Password = password;
+        }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+
+    }
+}
