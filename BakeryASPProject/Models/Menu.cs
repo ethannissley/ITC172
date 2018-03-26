4d66be43b6d9552099d82b365c7dc81551500688
ethannissley <ethannissley96@gmail.com>
2018-03-25 20:49:38 -0700

    Create Menu.cs

---
+++ BakeryASPProject/Models/Menu.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BakeryASP.Models
+{
+    public class Menu
+    {
+        public int ProductKey { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
