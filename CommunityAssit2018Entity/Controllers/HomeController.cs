eee4964b6cff7ddce2d737a141523d89c0a2b605
ethannissley <ethannissley96@gmail.com>
2018-03-25 18:55:45 -0700

    Create HomeController.cs

---
+++ CommunityAssit2018Entity/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssit2018Entity.Models;
+
+namespace CommunityAssit2018Entity.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+            return View(db.GrantTypes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
